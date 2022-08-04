856a6729b0b6a40d13e7038749b377e85b9716f9
Ixac <90751114+Ixac@users.noreply.github.com>
2022-08-04 14:15:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const namee = document.getElementById("p-name");
+const email = document.getElementById("p-email");
+const address = document.getElementById("p-address");
+const btn = document.getElementById("btn-random");
+const load = document.getElementById("div-loading-card");
+const icon = document.getElementById("span-gender-icon");
+const imgcard = document.getElementById("div-user-card");
+const gicon = document.getElementById("span-gender-icon");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  imgcard.src = resp.data.results.picture;
+  namee.value = resp.data.results.name;
+  email.value = resp.data.results.email;
+  address.value = resp.data.results.address;
 }
+callApi();
+
